734c5418b51016a48b71916d7bce33b9a60203e9
Josh Brito <jbrito6492@gmail.com>
2020-11-02 23:07:54 -0700

    changed asset image

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -1,17 +1,27 @@
-import logoImg from "../assets/logo.png";
+import logoImg from "../assets/player.png";
 
 export class SceneMain extends Phaser.Scene {
   constructor() {
     super({ key: "SceneMain" });
+    this.state = {}
   }
+
   preload() {
     this.load.image("SceneMain", logoImg);
   }
+
   create() {
-    this.image = this.add.image(400, 150, "SceneMain");
+    this.state.image = this.add.image(400, 150, "SceneMain");
     this.input.keyboard.on('keyup_D', function (event) {
-      this.image.x += 10;
+      this.state.image.x += 10;
     }, this);
+    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  };
+
+  update(delta) {
+    if (this.key_A.isDown) {
+      this.state.image.x--;
+    }
   }
 };
 
