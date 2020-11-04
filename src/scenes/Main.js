8ec0ac3301fd95986b8e5d15392eae821af569ce
Josh Brito <jbrito6492@gmail.com>
2020-11-04 01:40:04 -0700

    added animation to player png

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -1,6 +1,7 @@
 import { Scene } from "phaser";
 import button from "../assets/start-button.png";
 import background from "../assets/main-background.png";
+import player from "../assets/soldier/soldier_tilesheet.png";
 
 export default class MainScene extends Scene {
   constructor() {
@@ -10,6 +11,7 @@ export default class MainScene extends Scene {
   preload() {
     this.load.image("background", background);
     this.load.image("button", button);
+    this.load.spritesheet("player", player, { frameWidth: 73, frameHeight: 105, spacing: 7 , margin: 3});
   }
 
   create() {
@@ -21,12 +23,22 @@ export default class MainScene extends Scene {
     let button = this.add.sprite(0, 0, "button");
     button.y = this.sys.game.config.height / 2;
     button.x = this.sys.game.config.width / 2;
-    button.displayHeight = this.sys.game.config.height / 9;
-    button.displayWidth = this.sys.game.config.width / 9;
+    button.displayWidth = 100;
+    button.scaleY = button.scaleX;
     button.setInteractive().on(Phaser.Input.Events.POINTER_DOWN, () => {
       console.log('clicked');
-    })
-
+    });
+    this.char = this.add.sprite(100, 400);
+    this.anims.create({
+      key: "walk",
+      frames: [
+        { key: "player", frame: 9 },
+        { key: "player", frame: 10 },
+      ],
+      frameRate: 4,
+      repeat: -1
+    });
+    this.char.play("walk");
   };
 };
 
