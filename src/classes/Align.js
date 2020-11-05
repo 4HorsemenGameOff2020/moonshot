702c603cc231f6ff96e4434e19e27d2a2352d7e5
Josh Brito <jbrito6492@gmail.com>
2020-11-04 17:55:43 -0700

    created helper methods

--- src/classes/Align.js
+++ src/classes/Align.js
@@ -7,10 +7,10 @@ export default class Align {
     obj.x = game.config.width / 2;
     obj.y = game.config.height / 2;
   }
-  static centerH(obj) {
+  static centerV(obj) {
     obj.y = game.config.height / 2;
   }
   static centerH(obj) {
-    obj.y = game.config.height / 2;
+    obj.x = game.config.height / 2;
   }
 };
